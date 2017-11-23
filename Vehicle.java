4a4807adb35a0d1a8e33b06daa7ae2f2495c056f
GOKULAKRISHNAN.T <33505788+GOKULAKRISHNAN-T@users.noreply.github.com>
2017-11-23 12:00:30 +0530

    inheritance,hibernate with db

---
+++ Vehicle.java
@@ -0,0 +1,38 @@
+package org.veh;
+
+import javax.annotation.Generated;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name =" VEHL_TYPE",discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorValue("vehi")
+public class Vehicle {
+	@Id
+	@GeneratedValue
+	private int vehicleId;
+	private String name;
+	public int getVehicleId() {
+		return vehicleId;
+	}
+	public void setVehicleId(int vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Vehicle() {
+		super();
+	}
+}
+
